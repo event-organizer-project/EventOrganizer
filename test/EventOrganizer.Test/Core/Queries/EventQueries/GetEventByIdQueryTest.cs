48a79917c08a3b7c5cca640cece73cb3cf14562b
Mikita N <nuktinov@gmail.com>
2023-06-28 13:59:33 +0200

    MM-28

---
+++ test/EventOrganizer.Test/Core/Queries/EventQueries/GetEventByIdQueryTest.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using EventOrganizer.Core.Queries.EventQueries;
+using EventOrganizer.Core.Repositories;
+using EventOrganizer.Domain.Models;
+using Moq;
+
+namespace EventOrganizer.Test.Core.Queries.EventQueries
+{
+    public class GetEventByIdQueryTest
+    {
+        private GetEventByIdQuery underTest;
+
+        private Mock<IEventRepository> eventRepositoryMock;
+
+        public Mock<IMapper> mapperMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            eventRepositoryMock = new Mock<IEventRepository>();
+            mapperMock = new Mock<IMapper>();
+
+            underTest = new GetEventByIdQuery(eventRepositoryMock.Object, mapperMock.Object);
+        }
+
+        [Test]
+        public void Execute_Should_Throw_Exception()
+        {
+            eventRepositoryMock.Setup(x => x.Get(It.IsAny<int>()))
+                .Returns(null as EventModel);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                underTest.Execute(new GetEventByIdQueryParameters { Id = 1 }));
+        }
+    }
+}
